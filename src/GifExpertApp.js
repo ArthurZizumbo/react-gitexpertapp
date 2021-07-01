8c7de4d9479a1e858ceef3ca59f0a08984f596cc
jafed <artzizumbo@gmail.com>
2021-07-01 15:29:20 -0500

    Agrega el codigo a github

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = ( )=> {
+
+    const [categories, setCategories] = useState([ 'Dragon Ball Super' ]);
+
+    // const handleAdd =  ()=>{
+    //     // setCategories( [...categories, 'HunterXHunter'] );
+    //     setCategories( cats => [...cats, 'HunterXHunter'] );
+    // };
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory  setCategories = {setCategories} />
+            <hr></hr>
+            <ol>
+                {
+                    categories.map( category =>
+                        <GifGrid  
+                            key = { category }
+                            category = {category} 
+                        />
+                        // <li key = { category }> { category } </li> 
+                    )
+                }
+            </ol>
+        </>
+        
+    );
+
+};
+
+
